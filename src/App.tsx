904c0bed22ba8201e299062c7293d138c0075fbc
officeshinyujun <officeshinyujun@gmail.com>
2025-12-23 08:25:22 +0900

    inital

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from 'react-router-dom'
+import './App.css'
+import Main from './Pages/Main'
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Main />} />
+    </Routes>
+  )
+}
+
+export default App
---
+++ src/Pages/Main/index.tsx
@@ -0,0 +1,9 @@
+import styles from './style.module.scss';
+
+export default function Main() {
+  return (
+    <div className={styles.container}>
+      <h1>Main Page</h1>
+    </div>
+  );
+}
